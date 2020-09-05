5ecc8f5d1ca14905d0c53962b57c234211159465
assumednormal <maxwell.w.mitchell@gmail.com>
2020-09-05 16:12:21 -0400

    change host

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == "__main__":
-    app.run_server(port=int(os.environ["PORT"]))
+    app.run_server(host="0.0.0.0", port=int(os.environ["PORT"]))
